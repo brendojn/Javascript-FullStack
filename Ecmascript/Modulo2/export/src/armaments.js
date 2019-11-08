a1ceeadb58bfaa0e63df3d829e65686af3c6d50c
brendojn <brendojnio@gmail.com>
2019-11-08 07:49:21 -0300

    Concluido o EcmaScript

---
+++ Ecmascript/Modulo2/export/src/armaments.js
@@ -0,0 +1,5 @@
+export default {
+    laser: ["Phasers", "Torpedos de Fóton"],
+    explosion: ["Canhão", "Torpedos de TNT"]
+}
+
---
+++ Javascript/Modulo9/eventos_js/script.js
@@ -0,0 +1,4 @@
+document.querySelector("select[name='select-option'").addEventListener("change", function() {
+    alert(event.target.value)
+})
+
